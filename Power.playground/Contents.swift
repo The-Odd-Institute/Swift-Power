1b6de141888db8fe718922c5156415d2a851a7f5
Amir J <amir.jahan@gmail.com>
2025-10-14 07:22:17 -0700

    Minor code reformatting

--- Power.playground/Contents.swift
+++ Power.playground/Contents.swift
@@ -3,29 +3,20 @@ import UIKit
 // Define a new infix operator **
 infix operator **: MultiplicationPrecedence
 
-//** operator for types conforming Numeric and SignedNumeric
-func ** <T: Numeric & SignedNumeric>(lhs: T, rhs: Int) -> T
-{
+// ** operator for types conforming Numeric and SignedNumeric
+func ** <T: Numeric & SignedNumeric>(lhs: T, rhs: Int) -> T {
     var res: T = 1
-    if rhs > 0
-    {
-        for _ in 0..<rhs
-        {
+    if rhs > 0 {
+        for _ in 0..<rhs {
             res *= lhs
         }
-    }
-    else if rhs == 0
-    {
+    } else if rhs == 0 {
         res = 1
-    }
-    else
-    {
+    } else {
         fatalError("The ** operator is only implemented for positive powers")
     }
-    
+
     return res
 }
 
 let a = 3 ** 3
-
-
